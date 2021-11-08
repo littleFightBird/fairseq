b18142c80171cd14c6a966b849758889a84fc21b
姚卓远 <13820067387@163.com>
2021-11-08 21:19:53 +0800

    Update test.py

--- test.py
+++ test.py
@@ -39,7 +39,7 @@ if __name__=='__main__':
     audio_input = torch.randn((16,500))
     lengths = [random.randint(450,500) for i in range(16)]
     audio_mask = get_mask(audio_input, lengths)
-    text_input =  [ [np.randint(364) for  i in range(500)] for i in range(16)]
+    text_input =  [ [random.randint(364) for  i in range(500)] for i in range(16)]
     text_lengths = [500 for i in range]
     text_input, text_lengths, _ = collater_seq_label(text_input, task.dictionaries["phoneme"].pad())
     text_mask = get_mask(text_input, text_lengths)
