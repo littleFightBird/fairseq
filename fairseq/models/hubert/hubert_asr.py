04d9b08c7c853d6cdd2ff735f4fa5551179c6ece
姚卓远 <13820067387@163.com>
2021-11-09 14:19:03 +0800

    Update hubert_asr.py

--- fairseq/models/hubert/hubert_asr.py
+++ fairseq/models/hubert/hubert_asr.py
@@ -675,7 +675,7 @@ class HubertTextMTL(BaseFairseqModel):
         
         assert(audio_embedding.shape[1] == text_embedding.shape[1])
         # building mask
-        print(accum_alignment)
+        print(self.swap_embedding_ratio)
         bsz = audio_embedding.shape[0]
         for i in range(bsz):
             if self.swap_embedding_phoneme_aware:
