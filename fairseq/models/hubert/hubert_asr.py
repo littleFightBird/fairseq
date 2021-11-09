10975376ef96bef26681288882dc249253e8cc55
姚卓远 <13820067387@163.com>
2021-11-09 10:29:09 +0800

    Update hubert_asr.py

--- fairseq/models/hubert/hubert_asr.py
+++ fairseq/models/hubert/hubert_asr.py
@@ -707,6 +707,7 @@ class HubertTextMTL(BaseFairseqModel):
         accum_lists = []
         print(phoneme_seq.shape)
         print(phoneme_padding_mask.shape)
+        print([j for indice,j in enumerate(range(phoneme_seq[0].shape[0]))  ])
         for i in range(bsz):
             accum = [indice+1 for indice,j in enumerate(range(phoneme_seq[i].shape[0])) 
                 if phoneme_padding_mask[i][j] == False and phoneme_seq[i][j]!=phoneme_seq[i][j+1] ]
