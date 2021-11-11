8245adf00d3a37ed4f8a870ce4c1d2867f5f5c0e
姚卓远 <13820067387@163.com>
2021-11-11 13:22:12 +0800

    Update audio_text_dataset.py

--- fairseq/data/audio/audio_text_dataset.py
+++ fairseq/data/audio/audio_text_dataset.py
@@ -225,14 +225,10 @@ class AudioDataset(FairseqDataset):
         padd_mask = torch.BoolTensor((batch_size, max_size)).fill_(False)
         for  i, phoneme in enumerate(phoneme_target):
             diff = len(phoneme) - max_size
-            print(len(phoneme))
-            print(max_size)
-            print(diff)
-            print(padd_mask.shape)
             if diff == 0:
                 continue
             else:
-                padd_mask[i,diff:]=True
+                padd_mask[i,-diff:]=True
         return padd_mask
 
     def crop_to_max_size(self, wav, target_size):
